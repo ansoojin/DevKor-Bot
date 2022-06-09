34bdb1d3bf1dede58ec9dcde105bdae3c29028e5
seol <suljiwon@naver.com>
2022-06-10 05:02:15 +0900

    refactor: use summon function

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { summon } from './src/summon';
+
+dotenv.config();
+const port = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+const botName = 'DevKor';
+const keyword = '명';
+
+app.post('/', async (res) => {
+  try {
+    const { body } = res;
+    const { event, entity } = body;
+    const { plainText = '', personType = '', chatId: groupId } = entity;
+
+    const isPushEvent = event === 'push';
+    const hasKeyword = plainText.includes(keyword);
+    const isManager = personType === 'manager';
+
+    const needToSummon = isPushEvent && hasKeyword && isManager;
+
+    if (needToSummon) {
+      summon(plainText, keyword, groupId, botName);
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}...`);
+});
---
+++ src/summon.js
@@ -0,0 +1,48 @@
+import { sender } from "./constants/sender";
+import { sendMessage } from "./lib/sendMessage.js";
+import randomSelect from "./randomSelect.js";
+import celebrate from "./celebrate.js";
+import checkText from "./lib/checkText.js";
+
+const commonBody = (msg) => {
+    return {
+        blocks: [
+            {
+                type: "text",
+                value: msg,
+            },
+        ],
+        reactions: [
+            {
+                emojiName: "💥",
+                personKeys: ["string"],
+            }
+        ],
+        options: ["actAsManager"],
+    }
+};
+
+export const summon = (plainText, keyword, groupId, botName) => {
+    const [n, isInt, msg] = checkText(plainText, keyword);
+
+    if (msg) {
+      sendMessage(sender.GROUP, groupId, "message", {botName: botName},commonBody(msg), "post");
+    }
+    
+    if (n) {
+      let selectedManager = "";
+      let isFull = true;
+    
+      const num = isInt ? n : Math.floor(n);
+      for (let i = 0; i < num; i++) {
+        selectedManager = await randomSelect(groupId, botName);
+        celebrate(selectedManager, groupId, botName, isFull);
+      }
+
+      if (!isInt) {
+        isFull = false;
+        selectedManager = await randomSelect(groupId, botName);
+        celebrate(selectedManager, groupId, botName, isFull);
+      }
+    }
+}
