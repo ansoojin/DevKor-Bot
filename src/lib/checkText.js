d3eba812c6e411b1abdc73b400b707dba89486c8
seol <suljiwon@naver.com>
2022-06-10 05:02:39 +0900

    refactor: add check num to check text

---
+++ src/lib/checkText.js
@@ -0,0 +1,43 @@
+const checkNum = (string) => {
+  const num = Number(string);
+  if (!isNaN(num)) {
+    const isInt = num % 1 === 0 ? true : false;
+    return {
+      isInt: isInt,
+      num: num,
+    };
+  }
+
+  return {
+    isInt: false,
+    num: undefined,
+  };
+};
+
+const checkText = (plainText, keyword) => {
+  const textList = plainText.split(' ');
+  let n = undefined;
+  let msg = undefined;
+
+  const textLen = textList.length;
+  for (let i = textLen - 1; i >= 0; i--) {
+    let x = textList[i];
+
+    if (x.includes(keyword)) {
+      const text = x.split(keyword)[0];
+      const { isInt, num } = checkNum(text);
+
+      if (num < 0) {
+        msg = '😩 에휴.. 음수를 입력하는 바보가 어딨어. 내가 양수로 바꿔줄게^^ 다음부턴 잘해.';
+      } else if (!isInt) {
+        msg = '아 또 소수점..😤';
+        n = num;
+      } else {
+        n = num;
+      }
+      return [n, isInt, msg];
+    }
+  }
+};
+
+export default checkText;
