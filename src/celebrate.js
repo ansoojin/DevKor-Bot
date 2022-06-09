05e86cd64c6d8c8aa66bf7cd90efb9d90cd9cf73
seol <suljiwon@naver.com>
2022-06-10 05:01:59 +0900

    refactor: use send message function

--- src/celebrate.js
+++ src/celebrate.js
@@ -1,50 +1,33 @@
-import fetch from "node-fetch";
-import dotenv from "dotenv";
-dotenv.config();
+import { sendMessage } from './lib/sendMessage.js';
+import { sender } from './constants/sender';
 
-async function celebrate(auth, selectedManager, groupId, botName, isFull = true) {
-    try {
-        let msg = "";
-        if (!isFull) {
-            let halfName = "";
-            for (let i = 0; i < selectedManager.length; i++) {
-                if (i % 2 == 0) {
-                    halfName += selectedManager[i];
-                } else {
-                    halfName += "*";
-                }
-            }
-            msg = "🎉축하드립니다🎉 " + halfName + " 님이 당첨되었습니다!";
-        } else {
-            msg = "🎉축하드립니다🎉 " + selectedManager + " 님이 당첨되었습니다!";
-        }
-        const url = "https://api.channel.io/open/v5/groups/" + String(groupId) + "/messages?botName=" + botName;
-        const body = {
-            blocks: [
-                {
-                    type: "text",
-                    value: msg,
-                },
-            ],
-            options: ["actAsManager"],
-        };
-        const header = {
-            accept: "application/json",
-            "content-type": "application/json",
-            "x-access-key": auth["accessKey"],
-            "x-access-secret": auth["accessSecret"],
-        };
-        const response = await fetch(url, {
-            method: "post",
-            body: JSON.stringify(body),
-            headers: header,
-        });
-        const data = await response.json();
-    
-        console.log(data);
-    } catch (err) {
-        console.log(err);
+const celebrate = async (selectedManager, groupId, botName, isFull = true) => {
+  let msg = '';
+  if (!isFull) {
+    let halfName = '';
+    for (let i = 0; i < selectedManager.length; i++) {
+      if (i % 2 == 0) {
+        halfName += selectedManager[i];
+      } else {
+        halfName += '*';
+      }
     }
-}
+    msg = '🎉축하드립니다🎉 ' + halfName + ' 님이 당첨되었습니다!';
+  } else {
+    msg = '🎉축하드립니다🎉 ' + selectedManager + ' 님이 당첨되었습니다!';
+  }
+
+  const body = {
+    blocks: [
+      {
+        type: 'text',
+        value: msg,
+      },
+    ],
+    options: ['actAsManager'],
+  };
+
+  sendMessage(sender.GROUP, String(groupId), 'messages', { botName: botName }, body, 'post');
+};
 
 export default celebrate;
---
+++ src/randomSelect.js
@@ -0,0 +1,57 @@
+import { sendMessage } from './lib/sendMessage.js';
+import { sender } from './constants/sender';
+
+const getMembers = async (groupId) => {
+  let members = [];
+
+  const response = sendMessage(sender.GROUP, String(groupId), 'sessions', undefined, undefined, 'get');
+  const data = response.json();
+
+  await Promise.all(
+    data['sessions'].map(async (iter) => {
+      members.push(iter['personId']);
+    }),
+  );
+
+  return members;
+};
+
+const selectMember = (memberList) => {
+  let pickedIdx = 0;
+  let managerId = '';
+
+  pickedIdx = Math.floor(Math.random() * memberList.length);
+  managerId = memberList[pickedIdx];
+
+  return managerId;
+};
+
+const getMemberName = async (managerId) => {
+  const response = sendMessage(sender.MANAGER, String(managerId), '', undefined, undefined, 'get');
+  const data = await response.json();
+  return data['manager']['name'];
+};
+
+const personalAnnounce = async (managerId, botName) => {
+  const body = {
+    blocks: [
+      {
+        type: 'text',
+        value: '🎉당첨🎉 축하드립니다!',
+      },
+    ],
+    options: ['actAsManager'],
+  };
+
+  sendMessage(sender.ANNOUNCEMENTS, undefined, 'announce', { botName: botName, managerIds: String(managerId) }, body, 'post');
+};
+
+const randomSelect = async (groupId, botName) => {
+  const memberList = await getMembers(groupId);
+  const managerId = await selectMember(memberList);
+  const selectedManager = await getMemberName(managerId);
+  await personalAnnounce(managerId, botName);
+  return selectedManager;
+};
+
+export default randomSelect;
