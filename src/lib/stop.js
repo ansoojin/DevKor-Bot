f581220e28c2372cbdb85ada7529ae74ac072e24
aiccuracy <soojin.emily@gmail.com>
2022-06-11 18:35:00 +0900

    fix: remove duplicated express call

--- src/lib/stop.js
+++ src/lib/stop.js
@@ -1,6 +1,4 @@
 #!/usr/bin/env node
-import express from "express";
-
 const express = require("express");
 const app = express();
 const shell = require("shelljs");
@@ -8,7 +6,7 @@ const port = process.env.PORT;
 
 export const stop = () => {
     shell.cd('~');
-    
+
     if (shell.exec("heroku ps:scale web=0").code !== 0) {
         shell.echo("Error: Heroku server down failed");
         shell.exit(1);
