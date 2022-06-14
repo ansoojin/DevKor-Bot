970155b88cce417d3c6fb3af023045a47ac8497b
aiccuracy <soojin.emily@gmail.com>
2022-06-15 04:40:43 +0900

    feat: add news schedule at 9am

--- src/routes/news/schedule.js
+++ src/routes/news/schedule.js
@@ -3,16 +3,16 @@ import { getNews } from './scraping.js';
 import { sender } from '../../constants/sender.js';
 import { sendMessage } from '../../lib/sendMessage.js';
 
+const groupId = 146628;
+const botName = "DevKor";
 const rule = new schedule.RecurrenceRule();
 
 rule.dayOfWeek = [new schedule.Range(1, 6)];
-rule.hour = 4;
-rule.minute = 37;
+rule.hour = 9;
+rule.minute = 0;
 rule.tz = 'Asia/Seoul'
 
 export const newsScheduler = schedule.scheduleJob(rule, async () => {
-    const groupId = 167619;
-    const botName = "DevKor";
     const [newsUrl, newsTitle] = await getNews();
     const body = {
         blocks: [
